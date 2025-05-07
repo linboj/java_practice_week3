995ea9692abd5f83b32bc9bf5889292684159b3d
linboj <spps2215@gmail.com>
2025-05-07 11:24:48 +0800

    feat: add initializer for role and user init

---
+++ src/main/java/com/practice/ecommerce/initializer/DataInitializer.java
@@ -0,0 +1,60 @@
+package com.practice.ecommerce.initializer;
+
+import com.practice.ecommerce.entity.Role;
+import com.practice.ecommerce.entity.User;
+import com.practice.ecommerce.exception.GlobalExceptionHandler;
+import com.practice.ecommerce.repository.RoleRepository;
+import com.practice.ecommerce.repository.UserRepository;
+import jakarta.transaction.Transactional;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Component;
+
+import java.util.Set;
+
+@Component
+public class DataInitializer implements CommandLineRunner {
+
+    private static final Logger logger = LoggerFactory.getLogger(DataInitializer.class);
+
+    @Autowired
+    private RoleRepository roleRepository;
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    @Override
+    public void run(String... args) throws Exception {
+        if (roleRepository.count() == 0) {
+            Role userRole = roleRepository.findByName("ROLE_USER")
+                    .orElseGet(() -> roleRepository.save(new Role(null, "ROLE_USER", null)));
+
+            Role adminRole = roleRepository.findByName("ROLE_ADMIN")
+                    .orElseGet(() -> roleRepository.save(new Role(null, "ROLE_ADMIN", null)));
+
+            if (!userRepository.findByUsername("admin").isPresent()) {
+                User admin = new User();
+                admin.setName("admin");
+                admin.setEmail("admin@test.com");
+                admin.setPassword(passwordEncoder.encode("adminTEST")); // hashed password
+                admin.setRoles(Set.of(adminRole, userRole));
+                userRepository.save(admin);
+                logger.info("ADMIN USER CREATE: {}", admin);
+
+                User user = new User();
+                user.setName("testUser");
+                user.setEmail("user@test.com");
+                user.setPassword(passwordEncoder.encode("userTEST")); // hashed password
+                user.setRoles(Set.of(userRole));
+                userRepository.save(user);
+                logger.info("USER CREATE: {}", user);
+            }
+        }
+    }
+}
