6626964bda12328a3fd562a2e4b2a101be22d70b
linboj <spps2215@gmail.com>
2025-05-08 16:44:40 +0800

    fix: tests failed

--- src/test/java/com/practice/ecommerce/controller/ProductAdminControllerTest.java
+++ src/test/java/com/practice/ecommerce/controller/ProductAdminControllerTest.java
@@ -1,9 +1,12 @@
 package com.practice.ecommerce.controller;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.practice.ecommerce.config.SecurityConfig;
 import com.practice.ecommerce.controller.admin.ProductAdminController;
 import com.practice.ecommerce.dto.UpsertProductDTO;
 import com.practice.ecommerce.entity.Product;
+import com.practice.ecommerce.exception.CustomAccessDeniedHandler;
+import com.practice.ecommerce.exception.CustomAuthenticationEntryPoint;
 import com.practice.ecommerce.filter.JwtRequestFilter;
 import com.practice.ecommerce.repository.UserRepository;
 import com.practice.ecommerce.service.JwtService;
@@ -27,7 +30,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest(ProductAdminController.class)
-@Import({JwtRequestFilter.class, UserService.class, JwtService.class})
+@Import({SecurityConfig.class, JwtRequestFilter.class, UserService.class, JwtService.class, CustomAuthenticationEntryPoint.class, CustomAccessDeniedHandler.class})
 public class ProductAdminControllerTest {
     @Autowired
     private MockMvc mockMvc;
@@ -53,7 +56,7 @@ public class ProductAdminControllerTest {
 
         mockMvc.perform(post("/api/admin/products").contentType(MediaType.APPLICATION_JSON)
                         .content(objectMapper.writeValueAsString(product)))
-                .andExpect(status().isOk()).andExpect(jsonPath("$.success").value(true))
+                .andExpect(status().isCreated()).andExpect(jsonPath("$.success").value(true))
                 .andExpect(jsonPath("$.data.id").value(1))
                 .andExpect(jsonPath("$.data.name").value("TEST1"))
                 .andExpect(jsonPath("$.data.description").value("TEST1"));
@@ -65,7 +68,7 @@ public class ProductAdminControllerTest {
     @WithMockUser(username = "admin@test.com", roles = {"USER", "ADMIN"})
     public void updateProduct() throws Exception {
         Product updatedProduct = Product.builder()
-                .id(null).name("TEST1").description("TEST1").price(BigDecimal.valueOf(10)).stock(1).build();
+                .id(1L).name("TEST1").description("TEST1").price(BigDecimal.valueOf(10)).stock(1).build();
 
         when(productService.updateProduct(eq(1L), any(UpsertProductDTO.class))).thenReturn(updatedProduct);
 
--- src/test/java/com/practice/ecommerce/controller/ProductControllerTest.java
+++ src/test/java/com/practice/ecommerce/controller/ProductControllerTest.java
@@ -1,12 +1,29 @@
 package com.practice.ecommerce.controller;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.practice.ecommerce.config.CorsConfig;
+import com.practice.ecommerce.config.SecurityConfig;
+import com.practice.ecommerce.controller.admin.ProductAdminController;
+import com.practice.ecommerce.dto.UnifiedAPIResponse;
 import com.practice.ecommerce.entity.Product;
+import com.practice.ecommerce.exception.CustomAccessDeniedHandler;
+import com.practice.ecommerce.exception.CustomAuthenticationEntryPoint;
+import com.practice.ecommerce.exception.ErrorDetails;
+import com.practice.ecommerce.filter.JwtRequestFilter;
+import com.practice.ecommerce.repository.UserRepository;
+import com.practice.ecommerce.service.JwtService;
 import com.practice.ecommerce.service.ProductService;
+import com.practice.ecommerce.service.UserService;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.Import;
 import org.springframework.data.domain.*;
+import org.springframework.security.test.context.support.WithAnonymousUser;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -20,15 +37,19 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest(ProductController.class)
+@Import({SecurityConfig.class, JwtRequestFilter.class, UserService.class, JwtService.class, CustomAuthenticationEntryPoint.class, CustomAccessDeniedHandler.class})
 public class ProductControllerTest {
     @Autowired
     private MockMvc mockMvc;
 
-    @MockitoBean
+    @MockBean
     private ProductService productService;
 
     @Autowired
     private ObjectMapper objectMapper;
+
+    @MockitoBean
+    private UserRepository userRepository;
 
     @Test
     public void getProduct() throws Exception {
@@ -37,7 +58,7 @@ public class ProductControllerTest {
 
         when(productService.getProductById(1L)).thenReturn(product);
 
-        mockMvc.perform(get("/api/products/1")).andExpect(status().isOk())
+        mockMvc.perform(get("/api/public/products/1")).andExpect(status().isOk())
                 .andExpect(jsonPath("$.success").value(true))
                 .andExpect(jsonPath("$.data.id").value(1))
                 .andExpect(jsonPath("$.data.name").value("TEST1"))
@@ -54,23 +75,23 @@ public class ProductControllerTest {
                 Product.builder().id(1L).name("TEST1").description("TEST1").price(BigDecimal.valueOf(10)).stock(1).build()
         );
         Page<Product> expectedPage = new PageImpl<>(products);
-        int page = 1;
-        int size = 5;
+        int page = 0;
+        int size = 10;
         String sortBy = "name";
         Sort.Direction sortDirection = Sort.Direction.DESC;
         Pageable pageable = PageRequest.of(page, size, Sort.by(sortDirection, sortBy));
 
-        when(productService.getAllProducts(pageable)).thenReturn(expectedPage);
+        when(productService.getAllProducts(any(Pageable.class))).thenReturn(expectedPage);
 
-        mockMvc.perform(get("/api/products/1")).andExpect(status().isOk())
+        mockMvc.perform(get("/api/public/products")).andExpect(status().isOk())
                 .andExpect(jsonPath("$.success").value(true))
-                .andExpect(jsonPath("$.data[0].id").value(3))
-                .andExpect(jsonPath("$.data[0].name").value("TEST3"))
-                .andExpect(jsonPath("$.data[0].stock").value(2))
-                .andExpect(jsonPath("$.data[2].id").value(1))
-                .andExpect(jsonPath("$.data[2].name").value("TEST1"))
-                .andExpect(jsonPath("$.data[2].stock").value(1));
+                .andExpect(jsonPath("$.data.content[0].id").value(3))
+                .andExpect(jsonPath("$.data.content[0].name").value("TEST3"))
+                .andExpect(jsonPath("$.data.content[0].stock").value(2))
+                .andExpect(jsonPath("$.data.content[2].id").value(1))
+                .andExpect(jsonPath("$.data.content[2].name").value("TEST1"))
+                .andExpect(jsonPath("$.data.content[2].stock").value(1));
 
-        verify(productService, times(1)).getAllProducts(pageable);
+        verify(productService, times(1)).getAllProducts(any(Pageable.class));
     }
 }
